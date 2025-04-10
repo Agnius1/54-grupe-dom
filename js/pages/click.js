79e7ad8134e4f85e84c64127974bf101b2ac32a2
Agnius1 <you@example.com>
2025-04-10 16:19:07 +0300

    xjpjms

--- js/pages/click.js
+++ js/pages/click.js
@@ -1,20 +1,20 @@
-import { header } from '../components/header.js';
+import { header } from "../components/header.js";
 
 header();
 
-const countDOM = document.querySelector('.count');
-const btnMinusDOM = document.querySelector('.btn');
-const btnPlusDOM = document.querySelector('.btn:last-child');
+const countDOM = document.querySelector(".count");
+const btnMinusDOM = document.querySelector(".btn");
+const btnPlusDOM = document.querySelector(".btn:last-child");
 
 let sum = 0;
 
 function minus() {
-    countDOM.textContent = --sum;
+  countDOM.textContent = --sum;
 }
 
 function plus() {
-    countDOM.textContent = ++sum;
+  countDOM.textContent = ++sum;
 }
 
-btnMinusDOM.addEventListener('click', minus);
-btnPlusDOM.addEventListener('click', plus);
+btnMinusDOM.addEventListener("click", minus);
+btnPlusDOM.addEventListener("click", plus);
