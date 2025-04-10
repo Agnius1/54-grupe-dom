821e196e70ff388239edca81be7f90cea39cc279
Agnius1 <you@example.com>
2025-04-10 16:12:09 +0300

    nzn

--- js/components/header.js
+++ js/components/header.js
@@ -1,38 +1,35 @@
 export function header() {
-    let base = 'http://localhost:5408/';
-    let projectName = '';
-    if (location.hostname !== 'localhost') {
-        projectName = '/54-grupe-dom';
-        base = 'https://front-end-by-rimantas.github.io/54-grupe-dom/';
-    }
-    document.head.insertAdjacentHTML('afterbegin', `<base href="${base}">`);
+  if (location.hostname !== "localhost") {
+    document.head.insertAdjacentHTML("");
+  }
 
-    const menu = [
-        { text: 'Home', href: '/' },
-        { text: 'Text', href: '/text/' },
-        { text: 'Food', href: '/food/' },
-        { text: 'Darzas', href: '/darzas/' },
-        { text: 'Header', href: '/header/' },
-        { text: 'Click', href: '/click/' },
-        { text: 'Like', href: '/like/' },
-    ];
+  const menu = [
+    { text: "Home", href: "/54-grupe-dom/" },
+    { text: "Text", href: "/54-grupe-dom/text" },
+    { text: "Food", href: "/54-grupe-dom/food" },
+    { text: "Darzas", href: "/54-grupe-dom/darzas" },
+    { text: "Header", href: "/54-grupe-dom/header" },
+    { text: "Click", href: "/54-grupe-dom/click" },
+  ];
 
-    let linksHTML = '';
+  const lp = location.pathname;
+  const currentPage = lp.length > 1 && lp.at(-1) === "/" ? lp.slice(0, -1) : lp;
+  let linksHTML = "";
 
-    for (const link of menu) {
-        let activePage = '';
-        if (projectName + link.href === location.pathname) {
-            activePage = 'active';
-        }
-
-        linksHTML += `<a class="link ${activePage}" href=".${link.href}">${link.text}</a>`;
+  for (const link of menu) {
+    let activePage = "";
+    if (link.href === currentPage) {
+      activePage = "active";
     }
 
-    const HTML = `
+    linksHTML += `<a class="link ${activePage}" href="${link.href}">${link.text}</a>`;
+  }
+
+  const HTML = `
         <header class="main-header">
-            <img class="logo" src="./img/logo.png" alt="Logo">
+            <img class="logo" src="/54-grupe-dom/food/pizza.png" alt="Logo">
             <nav class="main-nav">${linksHTML}</nav>
         </header>`;
 
-    document.body.insertAdjacentHTML('afterbegin', HTML);
+  document.body.insertAdjacentHTML("afterbegin", HTML);
 }
